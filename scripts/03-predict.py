e036884db9a1590a4c43fc5c288d459a90613aba
suzikim <kimsuzi@kaist.ac.kr>
2019-04-27 06:52:47 +0900

    make prediction with each directory of shot

--- scripts/03-predict.py
+++ scripts/03-predict.py
@@ -9,17 +9,28 @@ from models.model_bce import ModelBCE
 import argparse
 
 
-def test(path_to_images, path_output_maps, model_to_test=None):
-    list_img_files = [k.split('/')[-1].split('.')[0] for k in glob.glob(os.path.join(path_to_images, '*'))]
-    # Load Data
-    list_img_files.sort()
-    for curr_file in tqdm(list_img_files, ncols=20):
-        print os.path.join(path_to_images, curr_file + '.jpg')
-        img = cv2.cvtColor(cv2.imread(os.path.join(path_to_images, curr_file + '.jpg'), cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
-        predict(model=model_to_test, image_stimuli=img, name=curr_file, path_output_maps=path_output_maps)
+def test(path_to_images, shot_counts, model_to_test=None):
+    for shot_idx in range(1, int(shot_counts)+1):
+        shot_dir = 'shot-%d' % shot_idx
+        shot_img_dir = os.path.join(shot_dir, 'frame_images')
+        shot_sal_dir = os.path.join(shot_dir, 'saliency_maps')
+
+        list_img_files = [k.split('/')[-1].split('.')[0] for k in glob.glob(os.path.join(path_to_images, shot_img_dir, '*'))]
+
+        print "predict %d-th shot from %s to %s: %d frames" % (shot_idx, shot_img_dir, shot_sal_dir, len(list_img_files))
+
+        # Load Data
+        list_img_files.sort()
+
+        for curr_file in tqdm(list_img_files, ncols=20):
+            curr_img_path = os.path.join(path_to_images, shot_img_dir, curr_file + '.jpg')
+            print curr_img_path
+
+            img = cv2.cvtColor(cv2.imread(curr_img_path, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+            predict(model=model_to_test, image_stimuli=img, name=curr_file, path_output_maps=shot_sal_dir)
 
 
-def main(input_dir, output_dir):
+def main(input_dir, shot_counts):
     # Create network
     model = ModelBCE(INPUT_SIZE[0], INPUT_SIZE[1], batch_size=8)
     
@@ -30,13 +41,13 @@ def main(input_dir, output_dir):
 
     print "Start predicting..."
     # Here need to specify the path to images and output path
-    test(path_to_images=input_dir, path_output_maps=output_dir, model_to_test=model)
+    test(path_to_images=input_dir, shot_counts=shot_counts, model_to_test=model)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--inputdir', help='Input directory', required=True)
-    parser.add_argument('-o', '--outputdir', help='Output directory', required=True)
+    parser.add_argument('-c', '--shotcounts', help='Shot Counts', required=True)
 
     args = parser.parse_args()
-    main(args.inputdir, args.outputdir)
+    main(args.inputdir, args.shotcounts)
